bfb4974d2961508efefd2a60d33460b264d68594
Michael Caron <michael.r.caron@gmail.com>
2010-05-05 21:50:24 -0500

    Implements Matrix button on the Preset view.

--- src/main/java/com/intelix/digihdmi/app/views/PresetLoadListView.java
+++ src/main/java/com/intelix/digihdmi/app/views/PresetLoadListView.java
@@ -1,29 +1,19 @@
 package com.intelix.digihdmi.app.views;
 
-import com.intelix.digihdmi.app.DigiHdmiApp;
-import java.awt.event.ActionEvent;
-import javax.swing.AbstractAction;
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
 import org.jdesktop.application.Application;
 
 public class PresetLoadListView extends ButtonListView {
 
+    @Override
     protected void initializeHomePanel() {
         super.initializeHomePanel();
 
         JButton matrixView = new JButton();
 
-        matrixView.setAction(new AbstractAction("MatrixView") {
-
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(((DigiHdmiApp) Application.getInstance()).getMainView().getFrame(), "Not so fast. This isn't implemented yet!");
-            }
-
-            public boolean isEnabled() {
-                return true;
-            }
-        });
+        matrixView.setAction(
+            Application.getInstance().getContext().getActionMap().get("showMatrixView")
+        );
         this.homePanel.add(matrixView);
     }
 }
